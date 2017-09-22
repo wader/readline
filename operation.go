05142cc0d63581f6be631a642556965ee1ce5ada
luis <stdiopt@gmail.com>
2017-09-22 03:40:22 -0300

    Added shiftTab in opSearch
    This will make move backwards in search list by
    pressing shift tab

--- operation.go
+++ operation.go
@@ -332,6 +332,8 @@ func (o *Operation) ioloop() {
 			isUpdateHistory = false
 			o.history.Revert()
 			o.errchan <- &InterruptError{remain}
+		case MetaShiftTab:
+			// do nothing
 		default:
 			if o.IsSearchMode() {
 				o.SearchChar(r)
