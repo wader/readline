f9a31b14ae61d71fdc4c75d6247742308ce7a4f2
luis <stdiopt@gmail.com>
2017-09-22 03:40:22 -0300

    Added shiftTab in opSearch
    This will make move backwards in search list by
    pressing shift tab

--- utils.go
+++ utils.go
@@ -54,6 +54,7 @@ const (
 	MetaDelete
 	MetaBackspace
 	MetaTranspose
+	MetaShiftTab
 )
 
 // WaitForResume need to call before current process got suspend.
@@ -103,6 +104,8 @@ func IsPrintable(key rune) bool {
 func escapeExKey(key *escapeKeyPair) rune {
 	var r rune
 	switch key.typ {
+	case 'Z':
+		r = MetaShiftTab
 	case 'D':
 		r = CharBackward
 	case 'C':
