a8bacec5b2278a1eebac727c551b0534f300b983
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-01-05 12:40:40 +0300

    golang.org/x/sys downgraded to golang.org/x/sys@release-branch.go1.11 for go1.11 compability

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.11
 require (
 	github.com/chzyer/logex v1.1.10 // indirect
 	github.com/chzyer/test v0.0.0-20180213035817-a1ea475d72b1
-	golang.org/x/sys v0.0.0-20210104204734-6f8348627aad
+	golang.org/x/sys v0.0.0-20180810173357-98c5dad5d1a0
 )
--- vendor/golang.org/x/sys/unix/gccgo.go
+++ vendor/golang.org/x/sys/unix/gccgo.go
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 
 // +build gccgo
-// +build !aix
 
 package unix
 
@@ -12,8 +11,10 @@ import "syscall"
 // We can't use the gc-syntax .s files for gccgo. On the plus side
 // much of the functionality can be written directly in Go.
 
+//extern gccgoRealSyscallNoError
 func realSyscallNoError(trap, a1, a2, a3, a4, a5, a6, a7, a8, a9 uintptr) (r uintptr)
 
+//extern gccgoRealSyscall
 func realSyscall(trap, a1, a2, a3, a4, a5, a6, a7, a8, a9 uintptr) (r, errno uintptr)
 
 func SyscallNoError(trap, a1, a2, a3 uintptr) (r1, r2 uintptr) {
