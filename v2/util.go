30e625cc9febe64e14cda58c9bd5e281fb865bf8
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-02-14 20:14:45 +0300

    added WaitForResume func

--- v2/util.go
+++ v2/util.go
@@ -1,5 +1,35 @@
 package readline
 
+import (
+	"sync"
+	"time"
+)
+
+// WaitForResume need to call before current process got suspend.
+// It will run a ticker until a long duration is occurs,
+// which means this process is resumed.
+func WaitForResume() chan struct{} {
+	ch := make(chan struct{})
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		ticker := time.NewTicker(10 * time.Millisecond)
+		t := time.Now()
+		wg.Done()
+		for {
+			now := <-ticker.C
+			if now.Sub(t) > 100*time.Millisecond {
+				break
+			}
+			t = now
+		}
+		ticker.Stop()
+		ch <- struct{}{}
+	}()
+	wg.Wait()
+	return ch
+}
+
 func correctErrNo0(e error) error {
 	// errno 0 means everything is ok :)
 	if e == nil || e.Error() == "errno 0" {
